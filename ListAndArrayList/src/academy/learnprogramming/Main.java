53bd876d424e962762e05ef0081234580913bdb6
tamphn <tmphncs@gmail.com>
2020-07-03 20:21:12 -0700

    initial commit

---
+++ ListAndArrayList/src/academy/learnprogramming/Main.java
@@ -0,0 +1,41 @@
+package academy.learnprogramming;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static int[] baseData = new int[10];
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Enter 10 integers: ");
+        getInput();
+        printArray(baseData);
+        resizeArray();
+        System.out.println("Enter 12 integers: ");
+        getInput();
+        printArray(baseData);
+    }
+
+    private static void getInput() {
+        for(int i = 0; i < baseData.length; i++) {
+            baseData[i] = scanner.nextInt();
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void resizeArray() {
+        int[] original = baseData;
+        baseData = new int[12];
+        for (int i = 0; i < original.length; i++) {
+            baseData[i] = original[i];
+        }
+    }
+}
